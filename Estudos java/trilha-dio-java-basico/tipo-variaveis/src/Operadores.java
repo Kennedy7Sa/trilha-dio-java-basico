8590ed95ee229e993a7264927768f9915da5ef40
Kennedy da Silva Arruda <156459108+Kennedy7Sa@users.noreply.github.com>
2024-05-04 11:46:00 -0300

    Bootcamp-santander

---
+++ Estudos java/trilha-dio-java-basico/tipo-variaveis/src/Operadores.java
@@ -0,0 +1,41 @@
+public class Operadores {
+    public static void main(String[] args) {
+//        //classe Operadores.java
+//        String nomeCompleto = "LINGUAGEM" +" " + "JAVA";
+//        System.out.println(nomeCompleto);
+
+
+        //qual o resultado das expressoes abaixo?
+//        String concatenacao ="?";
+//
+//        concatenacao = 1+1+1+"1";
+//        System.out.println(concatenacao);
+//
+//        concatenacao = 1+"1"+1+1;
+//        System.out.println(concatenacao);
+//
+//        concatenacao = 1+"1"+1+"1";
+//        System.out.println(concatenacao);
+//
+//        concatenacao = "1"+1+1+1;
+//        System.out.println(concatenacao);
+//
+//
+//        concatenacao = "1"+(1+1+1);
+//        System.out.println(concatenacao);
+
+
+        //*** x repetição usar operadores de interarção
+        int numero = 5;
+        //numero++;
+       // System.out.println(numero);
+        //no caso abaixo vamos somar incrementar 1
+       // System.out.println(++ numero);
+
+        //aqui vamos decrementar
+        System.out.println(numero --);
+
+
+    }
+
+}
---
+++ Estudos java/trilha-dio-java-basico/tipo-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+    //aqui foi usado um cast pra auterar o tipo
+        // para que ele possa entrar no numeroCurto2
+
+        //uma variavel constante ou imutavel é escrita em caixa alta com a palavra final no começo
+        final double VALOR_DE_PI = 3.14;
+    }
+}
