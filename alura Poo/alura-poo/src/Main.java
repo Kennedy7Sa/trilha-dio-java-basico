5f821b1a74851d931e3374bd8ec4a6468c45e4f9
Kennedy da Silva Arruda <156459108+Kennedy7Sa@users.noreply.github.com>
2024-05-07 11:46:16 -0300

    estudos iniciais em poo com alura

---
+++ alura Poo/alura-poo/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //para printar coisas na tela
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top gun Maverick");
+
+        //declarando variaveis
+        int anoDeLancamento = 2022;  // camelcase
+        System.out.println("Ano de lançamento "+ anoDeLancamento); // concatenando textos
+
+        boolean incluidoNoPlano = true;  // só aceita true ou false (verdadeiro e falso)
+        double notaDoFilme = 8.1; // valores com casas decimais
+
+
+
+
+
+
+    }
+}
---
+++ alura Poo/alura-poo/src/br/com/alura/exerciciosaulas/aula1/Subtracao.java
@@ -0,0 +1,8 @@
+package br.com.alura.exerciciosaulas.aula1;
+
+public class Subtracao {
+    public static void main(String[] args) {
+
+        System.out.println(10-5);
+    }
+}
