4c5b7ec159952e6dbe0841dea0959ca8485f2235
Kennedy7Sa <kennedy.arruda@aluno.unip.br>
2024-05-07 01:13:58 -0300

    App smartv

---
+++ Estudos java/trilha-dio-java-basico/tipo-variaveis/src/com/edu/metodos/Usuario.java
@@ -0,0 +1,23 @@
+package com.edu.metodos;
+
+public class Usuario {
+    public static void main(String[] args) {
+        //chamando a classe tv
+        SmarTv smartv = new SmarTv();
+
+        smartv.diminuirVolume();
+        smartv.diminuirVolume();
+        smartv.diminuirVolume();
+        smartv.aumentarVolume();
+
+        smartv.mudarCanal(13);
+
+
+        System.out.println("Tv ligada ? " + smartv.ligada);
+        System.out.println("Canal atual ? " + smartv.canal);
+        System.out.println("Volume atual  ? " + smartv.volume);
+
+        smartv.ligar();
+        System.out.println("novo status ,Tv ligada ? " + smartv.ligada);
+    }
+}
