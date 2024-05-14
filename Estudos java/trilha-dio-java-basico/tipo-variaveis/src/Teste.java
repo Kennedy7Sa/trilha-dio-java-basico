01eb751473bd9431ec45a80a61add521b2120731
Kennedy7Sa <kennedy.arruda@aluno.unip.br>
2024-05-14 08:50:25 -0300

    try/catch dio

---
+++ Estudos java/trilha-dio-java-basico/tipo-variaveis/src/Teste.java
@@ -0,0 +1,9 @@
+public class Teste {
+    public static void main(String[] args) {
+        boolean condicao = false;
+
+        while (condicao) {
+            System.out.println("executou ... ");
+        }
+    }
+}
